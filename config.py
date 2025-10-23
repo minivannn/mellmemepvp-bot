93c29e238c2e2b185b972fde621225459492df56
minivannn <vanekgg8@gmail.com>
2025-10-24 00:04:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = "8484983874:AAGUNIGQKcTCBkuWW1BqpgI6D2mBl3zoJWI"
+SHEET_ID = "1-qMfqwBm5ha9Fuwu0ZD8t61xEOZxm6aoa7dxEkKk_D0"
+GOOGLE_CREDENTIALS_JSON = {
+  "type": "service_account",
+  "project_id": "striking-theme-476020-b3",
+  "private_key_id": "db4e85680b642784b9be79cedd500178268a7b67",
+  "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCVXBmCnvJPTx5H\\nCFYGSqvSKzSC8W7wCgT+1mh/vn2JrqPi61+Uzbkul/J8R9/XMi8ajpj8oU8g2DxW\\nGvqLYr0874BpaOdgoFNRNYeqCQsjJsruvHVUUT+AsBsIzPrBGO71mDBDiH/UYgsF\\n1pRCpw+kBbVfLlQSEBDbdZDP5noY2zZKyJKxgw3/Wjour3aFrLAr6edQ9RspUoDU\\nDwuzVKXkHGIAHXH0vmtPwy8Fjd4YxCX8IiK9G2YuHdd/2fhTQ3itcqMhnQXiYoJN\\noeSBLEtXDWcsuesDjMP3Fbqez+PwQzegYSUUWX640qxPsZNHrHnf/LUWtczYVGLQ\\nHeIDmPkvAgMBAAECggEAIOTnQ2LBJ+FbYDaAR6NzYvab+oi0r43tWriazCOGX2fE\\ndUn0Q3/pXE8fmupzbGADirxECkN2e1vUMfyAGGmXFtACfnSZnKjCoLyC09ppBwVY\\npSqBRqAcNbnTQmVuhk949ULewkjSKlKrbc472dAUPEH7l2pLHq1GIScrzPXMByQO\\nDsjpvPrd3/jBiU0SNcmjR7pUSo2rpw3dHuNzkVX072mlaSkNBboh/aoVnPw03DuY\\nbRsmsDYc+1nrOTIhrFsbFMDS4mkBPxRC1lUIuANs0hMWL3DYBUQxgvhsE+o+obNV\\njvdH1d8YMCEBhCwm/3uoeg3uDLwxeOLD9J6PVltyOQKBgQDJRk+W14oNPqem4M4D\\nBzHhG4MhJtoGTMxjorDngQLQnrFx/jXYCPAzcgJj5Jhf8j4bYXzGcyB/ujlB7utk\\nUpaCJDSr8y6uoc8br0s/AWLySUrr1fkQfcX7kpOos0jDrrsVPshCX7alnr0VRPo/\\nDFVC5uQIShL4GVheJrTolVNc+QKBgQC9+ELy96TdDU7g2OXHVkqeFkm9M2Q7aqe4\\nvNQkqVcRxdHdm/b5vx/fj0kRK0Req1OYkEcPIPM+VDMTbCPerHCiJfRo51zeGfsi\\nZLJzJPSMT/H1NLvzPIeePSblA5l0s1RyDGBIcsym51GpN/fFigRghqKsD3Q+DiEi\\nepggrJxZZwKBgQCWHYGYwDLvg+5ocTxVV9Vkzi8z15U+o6LBIQMN9tORovo3SeEA\\nulHkHMEZndHjpXma1E/Kdu0jNlIkEbywe0RT1gsOv/XuOzKLtGbLnA4fJHjjghgb\\nq6Kw6faFGQpJIBXaV5PmpRpFVioyJdr6kQk6N5iVciHJtqWIuxesbErbiQKBgQC6\\nTkzSXh64kAGor+8BxNYfHjqpLEqDnwDoQ4BGiDEVBA3jKb9YhaFrrel5xhYiRiIR\\nAIZABw+nNAp3ZNn02ucHjvOZIjZIt/A8dccw97BCH3FrCpG9ZcKXZN/NP+9yrSEH\\nTK3sv3GHylqi+3mqqMXMKJ5G4k0dQtwqJVMm0EHxxwKBgD9cDJ0le8JDJhWem62+\\n+1VkvK98rNp941/bGxpG2+VqpVOc7tcWMI9tnCF3AfpghDkR4F9VLb90NS3IznhW\\nNtOP+X2S2/BarqH1lkDd07lqWJ8ZmOd1pofLl8gMPdLrjXkAV37ZmyB0Uq1DG630\\n5+yvPsuU17zHNhtOdYuYzJTD\\n-----END PRIVATE KEY-----\\n",
+  "client_email": "mellmemepvpv-bot@striking-theme-476020-b3.iam.gserviceaccount.com",
+  "client_id": "116181193911092432935",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mellmemepvpv-bot%40striking-theme-476020-b3.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
