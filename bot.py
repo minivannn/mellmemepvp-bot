677719b1cf439d97171dfbf1f90b764ab445af7d
minivannn <vanekgg8@gmail.com>
2025-10-23 23:37:51 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+from config import BOT_TOKEN
+from handlers import start, play, vote, leaderboard
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+start.register(dp)
+play.register(dp)
+vote.register(dp)
+leaderboard.register(dp)
+
+if name == "__main__":
+    executor.start_polling(dp)
